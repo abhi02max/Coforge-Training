c6c1ca72002bd99659abbc880c8e0542de1d0da7
Abhideep Bonigala <abhideep0204@gmail.com>
2026-07-18 16:26:51 +0530

    Add files via upload

---
+++ Day - 15,16 - SB-EMS/SB-EMS/SB-EMS/src/main/java/com/coforge/ems/SbEmsApplication.java
@@ -0,0 +1,14 @@
+package com.coforge.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //@configuration, @EnableAutoConfiguration, @ComponentScan
+public class SbEmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEmsApplication.class, args);
+		
+	}
+
+}
